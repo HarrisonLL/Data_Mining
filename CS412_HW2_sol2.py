70cc661341281aeaffff97e1d6365993390ce772
Liding <lidingl2@illinois.edu>
2019-03-20 19:42:05 -0500

    sol2-faster-for-large-dataset

--- CS412_HW2_sol2.py
+++ CS412_HW2_sol2.py
@@ -6,6 +6,7 @@ while True:
     except EOFError as error:
         break
 
+##variable declaration
 min_sup = 2
       
     
@@ -35,18 +36,21 @@ org_dict = {k:v for k,v in org_dict.items() if len(v)>= min_sup}
 
 #loop through length-2 to length-5 sequence
 the_dict = org_dict.copy()
-Uk = []
+Uk = {}
 for i in range(2,6) :
     candidates = candidate_generation(the_dict)
     if not candidates : break
     candidates = {k:v for k,v in candidates.items() if len(v)>= min_sup} 
-    for k,v in candidates.items():
-        Uk.append((k, len(v) ))
+    for elem in candidates :
+        Uk[elem] = len(candidates[elem])
     the_dict = candidates.copy()
     
+Uk_ = {}
+for k, v in Uk.items() :
+    Uk_[k] = v
 
-x = sorted(Uk, key=lambda element: (element[1]*-1,element[0]))[:20]
-for i in range(len(x)):
-    if i >=100:
-        break
-    print('[' + str(x[i][1])+', \'' + x[i][0]+'\']')
+sorted_list = [v[0] for v in sorted(Uk_.items(), key=lambda kv:(-kv[1],kv[0]))][:20]
+for i in range(len(sorted_list)) :
+    if i >= 100 : break
+    item = sorted_list[i]
+    print("[" + str(Uk_[item]) + ", " + "'" + item + "'" +"]")
